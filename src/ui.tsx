2e35c3486ad82138f28fb997c3211d515857fa13
cpalmer-ostmodern <chris.palmer@ostmodern.co.uk>
2023-01-31 17:13:21 +0000

    added the api request call

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,44 +1,56 @@
-import '!prismjs/themes/prism.css'
+import "!prismjs/themes/prism.css";
 
 import {
   Button,
   Container,
   render,
   VerticalSpace,
-  FileUploadButton
-} from '@create-figma-plugin/ui'
-import { emit } from '@create-figma-plugin/utilities'
-import { h } from 'preact'
-import { useCallback, useState } from 'preact/hooks'
-import { highlight, languages } from 'prismjs'
-import Editor from 'react-simple-code-editor'
-import styles from './styles.css'
-import { InsertCodeHandler } from './types'
+  FileUploadButton,
+} from "@create-figma-plugin/ui";
+import { emit } from "@create-figma-plugin/utilities";
+import { h } from "preact";
+import { useCallback, useState } from "preact/hooks";
+import { highlight, languages } from "prismjs";
+import Editor from "react-simple-code-editor";
+import styles from "./styles.css";
+import { InsertCodeHandler } from "./types";
 
 function Plugin() {
-  const [code, setCode] = useState(`insert JSON file here`)
+  const [code, setCode] = useState(`insert JSON file here`);
   const handleInsertCodeButtonClick = useCallback(
     async function () {
-        let request = new XMLHttpRequest()
-        // This link has random lorem ipsum text
-        request.open('GET', 'https://jsonplaceholder.typicode.com/todos/1')
-        request.responseType = 'json'
-        request.onload = () => {
-          console.log(request.response)
-        };
-        let data = await request.send()
-        return data;
-      // emit<InsertCodeHandler>('INSERT_JSON', code)
+      let body = JSON.stringify({
+        title: "foo",
+        body: "bar",
+        userId: 1,
+      });
+      let request = new XMLHttpRequest();
+      // This link has random lorem ipsum text
+      request.open("POST", "https://jsonplaceholder.typicode.com/posts", true);
+      request.setRequestHeader('Content-type', 'application/json');
+      request.onreadystatechange = function () {
+        //Call a function when the state changes.
+        if (request.readyState == 4 && request.status == 200) {
+          alert(request.responseText);
+        }
+      };
+      request.send(body);
+      request.responseType = "json";
+      request.onload = () => {
+        console.log(request.response);
+      };
+      let data = await request.send();
+      return data;
     },
     [code]
-  )
+  );
   return (
     <Container space="medium">
       <VerticalSpace space="small" />
       <div class={styles.container}>
         <Editor
           highlight={function (code: string) {
-            return highlight(code, languages.js, 'js')
+            return highlight(code, languages.js, "js");
           }}
           onValueChange={setCode}
           preClassName={styles.editor}
@@ -52,7 +64,7 @@ function Plugin() {
       </Button>
       <VerticalSpace space="small" />
     </Container>
-  )
+  );
 }
 
-export default render(Plugin)
+export default render(Plugin);
